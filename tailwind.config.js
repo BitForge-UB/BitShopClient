534fa3868321c7055fdbb78e8258802ce44bac50
BBroOW <brage_hagelund@outlook.com>
2024-04-17 14:06:11 +0200

    Update tailwind.config.js with new color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      Primary: "#004E98",
+      Secondary: "#FFA500",
+      Background: "#F5F5F5",
+    },
   },
+  extend: {},
   plugins: [],
-}
+};
