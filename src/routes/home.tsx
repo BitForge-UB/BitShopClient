08bbe0ad14d74352382af2184c3d23f2643d1fdb
BBroOW <brage_hagelund@outlook.com>
2024-04-17 14:58:45 +0200

    Add class-variance-authority and tailwind-merge dependencies

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "../assets/Components/Button";
 
-export const Route = createFileRoute('/home')({
-  component: () => <div>Hello /home!</div>
-})
+export const Route = createFileRoute("/home")({
+  component: () => (
+    <div>
+      <Button variant={"secondary"}>
+        <h1>HEI</h1>
+      </Button>
+    </div>
+  ),
+});
