112073ac979c506932511dedf2cc87359276786c
Kristian Haugsrud <kris.haugsrud@gmail.com>
2024-04-18 08:53:31 +0200

    Add react-icons package and update Navbar component

--- src/assets/Components/Navbar.tsx
+++ src/assets/Components/Navbar.tsx
@@ -1,9 +1,18 @@
-const navbar = () => {
+import React from 'react';
+import { Link } from '@tanstack/react-router';
+import { FiShoppingCart } from "react-icons/fi";
+
+const Navbar: React.FC = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className='bg-Primary flex justify-center relative h-12'>
+      <Link to="/" className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-Title'>
+        <h1>LOGO</h1>
+      </Link>
+      <Link to="/cart" className='absolute right-4 top-1/2 transform -translate-y-1/2 text-Title'>
+        <FiShoppingCart />
+      </Link>
+    </nav>
   );
 };
 
-export default navbar;
+export default Navbar;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "@tanstack/react-router";
 import { createRootRoute } from "@tanstack/react-router";
+import Navbar from "../assets/Components/Navbar";
 
 export const Layout: React.FC = () => {
   return (
     <div className="min-h-screen min-w-full">
+      <Navbar />
       <Outlet />
     </div>
   );
