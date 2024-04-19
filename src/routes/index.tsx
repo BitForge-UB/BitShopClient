4518d223af661b3d853b63cff6102b7afef37362
BBroOW <brage_hagelund@outlook.com>
2024-04-19 13:34:54 +0200

    Refactor Product component and update API call in productsApi.ts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,38 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useProducts } from "../hooks/useProduct";
+import Product from "../components/Product";
 
 export const Homepage: React.FC = () => {
+  const { isFetching, data, error } = useProducts(); // Example, replace with your own hook
+
+  if (isFetching) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  // Filter products with categoryName "Drikke"
+  const filteredProducts = data.filter(
+    (item: {
+      id: string;
+      title: string;
+      imagePath: string;
+      price: number;
+      categoryName: string;
+    }) => item.categoryName === "Drikke"
+  );
+
   return (
-    <div>
-      <h1>Welcome to the homepage!</h1>
-    </div>
+    <>
+      <div className="grid grid-cols-2 gap-4 m-4">
+        {filteredProducts.map((item) => (
+          <Product
+            name={item.title}
+            img={item.imagePath}
+            price={item.price}
+            key={item.id}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
