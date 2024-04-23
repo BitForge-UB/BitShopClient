6ecf9da11310819e6b0fa983c01335e1a6694c4b
Kristian Haugsrud <kris.haugsrud@gmail.com>
2024-04-23 13:08:09 +0200

    Refactor use of selectedIds in Homepage component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,16 @@ import { useProducts } from "../hooks/useProduct";
 import Product from "../components/Product";
 import { useEffect, useState } from "react";
 
+type Product = {
+  id: number;
+  title: string;
+  imagePath: string;
+  price: number;
+}
+
 export const Homepage: React.FC = () => {
   const { isFetching, data, error } = useProducts(); // Example, replace with your own hook
-  const [selectedIds, setSelectedIds] = useState<number[]>(JSON.parse(localStorage.getItem("selectedIds") || "[]"));
+  const [selectedIds, setSelectedIds] = useState<Product[]>(JSON.parse(localStorage.getItem("selectedIds") || "[]"));
 
   if (isFetching) return "Loading...";
 
@@ -24,27 +31,34 @@ export const Homepage: React.FC = () => {
     }) => item.categoryName === "Drikke"
   );
 
-  const onSelect = (id: number) => {
-    localStorage.setItem("selectedIds", JSON.stringify(selectedIds.includes(id)
-    ? selectedIds.filter((selectedId) => selectedId !== id)
-    : [...selectedIds, id]));
-    
-    
-    setSelectedIds(
-      selectedIds.includes(id)
-        ? selectedIds.filter((selectedId) => selectedId !== id)
-        : [...selectedIds, id]
-    );
+  /*
+   [
+    {
+      id: 1,
 
+    }
+   ]
+  */
+
+  const onSelect = (product: Product) => {
+    const isSelected = selectedIds.includes(product);
+
+    if (isSelected) {
+      setSelectedIds(selectedIds.filter((item) => item.id !== product.id));
+    } else {
+      setSelectedIds([...selectedIds, product]);
+    }
+
+    localStorage.setItem("selectedIds", JSON.stringify(selectedIds));
     // store it to localstorage
   }
 
   return (
     <>
       <div className="grid grid-cols-2 gap-4 m-4">
-        {filteredProducts.map((item) => (
+        {filteredProducts.map((item: Product) => (
           <div
-            onClick={() => onSelect(item.id)}
+            onClick={() => onSelect(item)}
             key={item.id}
           >
             <Product
